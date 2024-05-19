7be441fc3fa255d921fc85145dec65fc835d5232
Le Duc Duy <duyleduc123@gmail.com>
2024-05-19 12:06:18 +0700

    update

--- src/base/base.cpp
+++ src/base/base.cpp
@@ -6,5 +6,18 @@ using namespace std;
 
 base::base()
 {
+    if (csv.mmap("foo.csv"))
+    {
+        const auto header = csv.header();
+        for (const auto row : csv)
+        {
+            for (const auto cell : row)
+            {
+                // Do something with cell value
+                // std::string value;
+                // cell.read_value(value);
+            }
+        }
+    }
     cout << "Hello form base\n";
 }
